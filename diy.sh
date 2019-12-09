6fdd6bcadb5aaca970289d8ad97ce99854336220
benjaminbnuz <1411030061@mail.bnuz.edu.cn>
2019-12-10 06:06:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,3 +8,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/Leo-Jo/luci-app-ssr-plus-jo package/lean/luci-app-ssr-plus-jo
+git clone https://github.com/Ameykyl/my package/lean/myssr
