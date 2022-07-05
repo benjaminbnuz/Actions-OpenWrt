84e96ec5749ca97b25a8b2fbac494754064d5d5c
benjaminbnuz <1411030061@mail.bnuz.edu.cn>
2022-07-05 09:07:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,4 +9,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #git clone https://github.com/Leo-Jo/luci-app-ssr-plus-jo package/lean/luci-app-ssr-plus-jo
 #git clone https://github.com/Ameykyl/my package/lean/myssr
-git clone https://github.com/vernesong/OpenClash package/lean/luci-app-ssr-plus
+# git clone https://github.com/vernesong/OpenClash package/lean/luci-app-ssr-plus
